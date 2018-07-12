b99b0fad6b0237d44c9ad97b24bdb190ad4a2108
Jessica Ramirez <jessicaramirez330@gmail.com>
2018-07-12 16:50:48 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -11,17 +11,17 @@ if cents >= 0.25
 
 elsif cents  >= 0.10
 	dimes = cents / 0.10
-	coins[:dimes] = dimes 
+	coins[:dimes] = dimes.to_i 
 	cents = cents % 0.10
  
 elsif cents >= 0.05
 	nickeles = cents / 0.05
-	coins[:nickeles] = nickeles
+	coins[:nickeles] = nickeles.to_i
 	cents = cents % 0.05
 
 elsif cents >= 0.01
 	pennies = cents / 0.01
-	coins[:pennies] = pennies
+	coins[:pennies] = pennies.to_i
 	cents = cents % 0.01
 end
 return coins 
