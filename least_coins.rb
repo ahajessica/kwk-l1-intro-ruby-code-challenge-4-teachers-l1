d674c60cb99313d9afb4f4138079fea32fb21591
Jessica Ramirez <jessicaramirez330@gmail.com>
2018-07-12 16:58:02 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -7,7 +7,7 @@ coins = {:quarters => 0, :dimes => 0, :nickeles => 0, :pennies => 0}
 if cents >= 0.25
 	quarters = cents / 0.25
 	coins[:quarters] = quarters.to_i 
-	cents = cents % 0.25
+	cents = cents % 0.25.to_f
 
 elsif cents  >= 0.10
 	dimes = cents / 0.10
