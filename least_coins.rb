3aaf17ce9213c405dced1f12aa253871b927b92a
Jessica Ramirez <jessicaramirez330@gmail.com>
2018-07-12 16:44:18 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -4,25 +4,25 @@ def least_coins(cents)
 #Code your answer here!
 coins = {:quarters => 0, :dimes => 0, :nickeles => 0, :pennies => 0} 
 
-if cents >=0.25
-	quarters = cents / 0.25
+if cents >= 25
+	quarters = cents / 25
 	coins[:quarters] = quarters
-	cents = cents % 0.25
+	cents = cents % 25
 
-elsif cents  >= 0.10
-	dimes = cents / 0.10
+elsif cents  >= 10
+	dimes = cents / 10
 	coins[:dimes] = dimes 
-	cents = cents % 0.10
+	cents = cents % 10
  
-elsif cents >= 0.05
-	nickeles = cents / 0.05
+elsif cents >= 05
+	nickeles = cents / 05
 	coins[:nickeles] = nickeles
-	cents = cents % 0.05
+	cents = cents % 05
 
-elsif cents >= 0.01
-	pennies = cents / 0.01
+elsif cents >= 01
+	pennies = cents / 01
 	coins[:pennies] = pennies
-	cents = cents % 0.01
+	cents = cents % 01
 end
 return coins 
 end
