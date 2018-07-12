52a8a78905f314e06981d3b74f0ee689a6af529b
Jessica Ramirez <jessicaramirez330@gmail.com>
2018-07-12 16:08:31 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,26 @@
 #write out your code here
 
 def least_coins(cents)
-
 #Code your answer here!
+
+if change >= 1.00:
+	bills = change // 1.00
+	change = change % 1.00
+
+if change >=0.25:
+	quarters = change // 0.25
+	change = change % 0.25
+
+if change  >= 0.10:
+	dimes = change // 0.10
+	change = change % 0.10
+
+if change >= 0.05:
+	nickeles = change // 0.05
+	change = change % 0.05
+
+if change >= 0.01:
+	pinnies = change // 0.01
+	change = change % 0.01
 
 end
